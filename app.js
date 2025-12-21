1e456e9e4ff46347c0acb30c0b790a86223211de
hugh.han78 <hugh.han78@gmail.com>
2025-12-21 03:51:34 +0000

    commit-8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Cloud Build!'));
+app.get('/', (req, res) => res.send('HUGH Hello Cloud Build!'));
 
 app.listen(port, () => console.log(App listening on port ${port}));
 
